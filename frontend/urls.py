bc53a1db483c75d4c0b2567cf6843898814bec82
Igor Grosu <igrgro@gmail.com>
2023-02-12 22:53:43 +0200

    enhanced routes

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,7 +3,18 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path(r'<string>', views.home, name = 'home'),
-    path(r'<string>/<string1>', views.home, name = 'home'),
-    path(r'<string>/<string1>/<string2>', views.home, name = 'home')
+    path('picker', views.home, name = 'home'),
+    path('calculator', views.home, name = 'home'),
+    path('calculator/help', views.home, name = 'home'),
+    path('trends', views.home, name = 'home'),
+    path('trends/decades', views.home, name = 'home'),
+    path('trends/decades/<string>', views.home, name = 'home'),
+    path('trends/years', views.home, name = 'home'),
+    path('schemes', views.home, name = 'home'),
+    path('schemes/<string>', views.home, name = 'home'),
+    path('models', views.home, name = 'home'),
+    path('profile', views.home, name = 'home'),
+    path('log_in', views.home, name = 'home'),
+    path('sign_up', views.home, name = 'home'),
+    path('restore', views.home, name = 'home'),
 ]
