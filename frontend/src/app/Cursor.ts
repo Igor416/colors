2cf1f41ceff4773ac2b4c4a129c979d31f8608a3
Igor Grosu <igrgro@gmail.com>
2023-02-19 15:23:20 +0200

    Update Cursor.ts

--- frontend/src/app/Cursor.ts
+++ frontend/src/app/Cursor.ts
@@ -4,23 +4,22 @@ export class Cursor {
   x!: number;
   y!: number;
   canvasSize!: number;
-  radius!: number;
+  radius: number = 6; //for displaying
   color!: Color;
 
   constructor (x: number, y: number, size: number) {
-    const radius = 6; //for displaying
-    this.updateCoords(x, y);
-    this.canvasSize = size;
-    this.radius = radius;
-  }
-
-  updateCoords(x: number, y: number) {
     this.x = x;
     this.y = y;
+    this.canvasSize = size;
   }
 
   setCoords(c: number, angle: number) {
     this.x = c * Math.cos(angle * Math.PI / 180);
     this.y = c * Math.sin(angle * Math.PI / 180);
+  }
+
+  invertCoords(initial: Cursor) {
+    this.x = -initial.x
+    this.y = -initial.y
   }
 }
